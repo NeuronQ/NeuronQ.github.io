8ca300b45c354419a2c0258bfe13c2b6ec996221
NeuronQ <io@neuronq.ro>
2016-03-13 10:42:50 +0200

    publish: toggleable side menu bar
    generated from commit 3c609e57859566e10d3f7fd5bf3c2cbc04cbcfbb

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
-    'use strict';
+  'use strict';
 
-    $('.nav-links > li').each(function () {
-        $(this).height($(this).find('a').outerWidth());
-    });
+  $('.nav-links > li').each(function () {
+      $(this).height($(this).find('a').outerWidth());
+  });
+
+  setTimeout(function () {
+    $('body').removeClass('expanded-nav');
+  }, 4000);
+
+  $('.toggle-sidebar').on('click', function () {
+    $('body').toggleClass('expanded-nav');
+  });
 });
