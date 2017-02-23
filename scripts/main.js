9fad3805e49dfc8f9beb87b5f2fa28644cbdbb95
NeuronQ <io@neuronq.ro>
2017-02-23 12:39:18 +0100

    publish: latest updates and fixes, almost ready to be online
    generated from commit 19cbf801b21bae00af67d2524091604e6c0e6bdd

--- scripts/main.js
+++ scripts/main.js
@@ -12,4 +12,17 @@ $(document).ready(function () {
   $('.toggle-sidebar').on('click', function () {
     $('body').toggleClass('expanded-nav');
   });
+  
+  $('.do-expand-or-collapse-all-skills').on('click', function (e) {
+    e.preventDefault();
+    if ($(this).find('.text-collapse').hasClass('hide')) {
+      $('section.skills .collapsible-header:not(.active)').click();
+      $(this).find('.text-expand').addClass('hide')
+      $(this).find('.text-collapse').removeClass('hide')
+    } else {
+      $('section.skills .collapsible-header.active').click();
+      $(this).find('.text-expand').removeClass('hide')
+      $(this).find('.text-collapse').addClass('hide')
+    }
+  })
 });
